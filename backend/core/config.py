2b1336783e10801be83d6790cadc01da3d77f61d
aoxuexinghuo <1626297770@qq.com>
2025-07-17 08:34:59 +0800

    chore: config

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,8 +4,8 @@ class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_PORT = 3306
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '13579OKOK'
-    MYSQL_DATABASE = 'trade_db'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DATABASE = 'shop'
     
     # 生成连接字符串
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
@@ -14,8 +14,8 @@ MYSQL_CONFIG = {
     'host': 'localhost',
     'port': 3306,
     'user': 'root',
-    'password': '13579OKOK',
-    'database': 'trade_db',
+    'password': 'root',
+    'database': 'shop',
     'charset': 'utf8mb4'
 }
 
